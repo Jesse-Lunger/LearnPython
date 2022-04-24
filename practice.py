01d1359f5d5a2de1d3331fd97c92d9ff954aad0b
Jesse Lunger <jesselunger@gmail.com>
2022-04-23 17:54:15 -0700

    uploading input.txt and practice.py

---
+++ practice.py
@@ -0,0 +1,31 @@
+'''
+Todo:
+collect data from input.txt into a nested list/2d array [[row data],[row data], ....]
+input txt must be in current working directory.
+
+Wite a function that takes a nested list of any size and determines if there are multiples
+of any number, either in the row or the column
+
+Will need to look up:
+open, close, split, append
+
+refresher:
+myList = [a,b, [c, d]]
+mylist[0] = a
+mylist[1] = b
+mylist[2] = [c, d]
+mylist[2][0] = c 
+'''
+
+def collectInput(fileName):
+    '''
+    return nest list
+    '''
+    pass
+
+
+
+def findMultiples2dArray(arr):
+    '''
+    '''
+    pass
